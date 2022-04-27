af21596dc35f646d0913a3e2d2d8265f74df39fa
Diaci <davide.iaci@icloud.com>
2022-04-27 18:52:51 +0200

    Create class

--- src/main/java/StringScanner.java
+++ src/main/java/StringScanner.java
@@ -4,4 +4,35 @@
 
 public class StringScanner {
 
+    String[] words; // contain all words of the phrase
+
+    public StringScanner() {
+        words = null;
+    }
+
+    // split the phrase in words
+    private String[] phraseSplit() {
+        String[] tempWords = null;
+
+        // TODO
+
+        return tempWords;
+    }
+
+    // identify the type of conditional
+    private void conditionalScan() {
+        // TODO Trovare verbo base e a esclusione verificare ogni conditional
+    }
+
+    // TODO **Idea Scan** Controllare l’index della parola ”if”, se e’ 0 si fa in un modo, se e’ altro in un altro modo.
+
+    // get and set
+
+    public String[] getWords() {
+        return words;
+    }
+
+    public void setWords(String[] words) {
+        this.words = words;
+    }
 }
