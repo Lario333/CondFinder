6cd235e61e7965f8906791515bee8bb6750a299b
Lario <alessio.zarola@protonmail.com>
2022-04-28 15:42:25 +0200

    Next Fix -> comma

--- src/main/java/StringScanner.java
+++ src/main/java/StringScanner.java
@@ -9,7 +9,6 @@ import java.util.Map;
 public class StringScanner {
 
     private String[] phrase; // contain all words of the phrase
-    public String conditionalForm;
     private boolean ifStart;
     // default constructor
     public StringScanner() {
@@ -28,9 +27,50 @@ public class StringScanner {
             return false;
         }
     }
+    // Searches for abbreviations like "'d" , "'ll" , "'ve" and split them clearing the phrase for the scan
+    private String[] clearPhrase(){
+        String[] clearPhrase = new String[phrase.length + 6];
+        int counter = 0;
+        for(int i = 0 ; i < phrase.length; i++){
+            if (phrase[i].contains("'ll") || phrase[i].contains("'d") || phrase[i].contains("'ve") ){
+                if(phrase[i].contains("'ll")){
+                    clearPhrase[counter] = phrase[i].substring(0 , phrase[i].indexOf('\''));
+                    counter++;
+                    clearPhrase[counter] = "will";
+                    counter++;
+                } else if (phrase[i].contains("'d")){
+                    clearPhrase[counter] = phrase[i].substring(0 , phrase[i].indexOf('\''));
+                    counter++;
+                    clearPhrase[counter] = "would"; // would / had
+                    counter++;
+                } else if (phrase[i].contains("'ve")) {
+                    clearPhrase[counter] = phrase[i].substring(0, phrase[i].indexOf('\''));
+                    counter++;
+                    clearPhrase[counter] = "have";
+                    counter++;
+                }
+
+            } else {
+                clearPhrase[counter] = phrase[i];
+                counter++;
+            }
+        }
+        for(int i = 0 ; i < clearPhrase.length; i++){
+            System.out.print(clearPhrase[i] + " ");
+        }
+        System.out.println();
+
+        String[] finalPhrase = new String[counter];
+        for(int i = 0 ; i < counter ; i++){
+            finalPhrase[i] = clearPhrase[i];
+        }
+
+        return finalPhrase;
+    }
 
     private String[] getVerbalTenses(String phraseGiven) {
         this.phrase = phraseSplit(phraseGiven);
+        this.phrase = clearPhrase();
         this.ifStart = doesPhraseStartsWithIf(phrase); // getting if the phrase starts with if
         String[] phraseVerbs = new String[4];
         int verbCounter = 0;
@@ -43,16 +83,19 @@ public class StringScanner {
                 // runs if the last verb wasn't a composed tense
                 if (!skipVerb) {
                     // Enters the statement if the tense is past perfect
-                    if (phrase[i].equalsIgnoreCase("had")) {
+                    if (phrase[i].equalsIgnoreCase("had") || phrase[i].equalsIgnoreCase("hadn't")) {
                         // Enters in the statement if the word is a verb
                         if (Main.verbDB.isVerbContained(phrase[i + 1])) {
                             // Getting the verbal tense and putting it into the phraseVerbs array
                             phraseVerbs[verbCounter] = Main.verbDB.getVerbalTense(phrase[i] + " " + phrase[i + 1]);
                             verbCounter++;
                             skipVerb = true;
+                        } else{
+                            phraseVerbs[verbCounter] = Main.verbDB.getVerbalTense("had");
+                            verbCounter++;
                         }
                         // Enters if there's a "will" , "can" , "must" or [ imperative form < to-do ]
-                    } else if (phrase[i].equalsIgnoreCase("will") || phrase[i].equalsIgnoreCase("can") || phrase[i].equalsIgnoreCase("must")) {
+                    } else if (phrase[i].equalsIgnoreCase("will") || phrase[i].equalsIgnoreCase("can't") || phrase[i].equalsIgnoreCase("won't")  || phrase[i].equalsIgnoreCase("can") || phrase[i].equalsIgnoreCase("mustn't") || phrase[i].equalsIgnoreCase("must")) {
                         // if the word after is a verb in base form
                         if (Main.verbDB.isVerbContained(phrase[i + 1])) {
                             phraseVerbs[verbCounter] = "w/c/m-baseForm"; // Prob 1st conditional
@@ -60,7 +103,7 @@ public class StringScanner {
                             skipVerb = true;
                         }
                         // Enters if there's a "could" or "would"
-                    } else if (phrase[i].equalsIgnoreCase("could") || phrase[i].equalsIgnoreCase("would")) {
+                    } else if (phrase[i].equalsIgnoreCase("could") || phrase[i].equalsIgnoreCase("would") || phrase[i].equalsIgnoreCase("wouldn't") || phrase[i].equalsIgnoreCase("couldn't")) {
                         // Checks if there's a verb after could/would
                         if (Main.verbDB.isVerbContained(phrase[i + 1])) {
                             if (phrase[i + 1].equalsIgnoreCase("have") && Main.verbDB.getVerbalTense(phrase[i + 2]).equals("pstParticiple")) {
@@ -100,6 +143,9 @@ public class StringScanner {
                             phraseVerbs[verbCounter] = Main.verbDB.getVerbalTense(phrase[i] + " " + phrase[i + 1]);
                             verbCounter++;
                             skipVerb = true;
+                        } else{
+                            phraseVerbs[verbCounter] = Main.verbDB.getVerbalTense("had");
+                            verbCounter++;
                         }
                         // Enters if there's a "will" , "can" , "must" or [ imperative form < to-do ]
                     } else if (phrase[i].equalsIgnoreCase("will") || phrase[i].equalsIgnoreCase("can") || phrase[i].equalsIgnoreCase("must")) {
@@ -149,21 +195,46 @@ public class StringScanner {
     // identify the type of conditional and searches for the conditional tense used
     public String scanString(String phraseGiven) {
         String[] verbalTenses = getVerbalTenses(phraseGiven);
-        if (verbalTenses[0].equals("baseForm") && verbalTenses[1].equals("baseForm")){
-            return "Zero";
-        } else if (verbalTenses[0].equals("baseForm") && verbalTenses[1].equals("w/c/m-baseForm") ){
-            return "First";
-        }
-        return null;
-    }
 
-    // Get Conditional Form
-    public String getConditionalForm(){
-        if (conditionalForm != null){
-            return conditionalForm;
+        if (ifStart){
+            if (verbalTenses[0].equals("baseForm")){
+                if (verbalTenses[1].equals("baseForm")){
+                    return "Zero";
+                } else if (verbalTenses[1].equals("w/c/m-baseForm")){
+                    return "First";
+                }
+            } else if (verbalTenses[0].equals("smplPast")){
+                if (verbalTenses[1].equals("c/w-baseForm")){
+                    return "Second";
+                }
+            } else if (verbalTenses[0].equals("pstPerfect")){
+                if ( verbalTenses[1].equals("c/w-h-pstParticiple")){
+                    return "Third";
+                } else if (verbalTenses[1].equals("c/w-baseForm")){
+                    return "Mixed";
+                }
+            }
         } else {
-            return "conditional not found";
+            if (verbalTenses[1].equals("baseForm")){
+                if (verbalTenses[0].equals("baseForm")){
+                    return "Zero";
+                } else if (verbalTenses[0].equals("w/c/m-baseForm")){
+                    return "First";
+                } else if (verbalTenses[1].equals("smplPast")){
+                    if (verbalTenses[0].equals("c/w-baseForm")){
+                        return "Second";
+                    }
+                } else if (verbalTenses[1].equals("pstPerfect")){
+                    if ( verbalTenses[0].equals("c/w-h-pstParticiple")){
+                        return "Third";
+                    } else if (verbalTenses[0].equals("c/w-baseForm")){
+                        return "Mixed";
+                    }
+                }
+            }
         }
+
+        return "Conditional not found";
     }
 
     // Word get and set
